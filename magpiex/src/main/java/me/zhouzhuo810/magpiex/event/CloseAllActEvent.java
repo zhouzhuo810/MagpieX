63d0ffa5351294786a0674508680794c1b5ac82c
周卓 <244570672@qq.com>
2021-06-15 14:29:11 +0800

    部分代码添加注释；

--- magpiex/src/main/java/me/zhouzhuo810/magpiex/event/CloseAllActEvent.java
+++ magpiex/src/main/java/me/zhouzhuo810/magpiex/event/CloseAllActEvent.java
@@ -2,19 +2,22 @@ package me.zhouzhuo810.magpiex.event;
 
 /**
  * EventBus关闭所有Activity的事件
+ *
+ * @author zhouzhuo810
+ * @date 6/15/21 2:28 PM
  */
 public class CloseAllActEvent {
-
+    
     private boolean defaultAnim;
-
+    
     public CloseAllActEvent(boolean defaultAnim) {
         this.defaultAnim = defaultAnim;
     }
-
+    
     public boolean isDefaultAnim() {
         return defaultAnim;
     }
-
+    
     public void setDefaultAnim(boolean defaultAnim) {
         this.defaultAnim = defaultAnim;
     }
